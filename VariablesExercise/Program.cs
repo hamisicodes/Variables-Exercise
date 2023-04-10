9952b543846565b9ffe74f998b2bda11ac1366f0
Hamisi Ngao <newhamisi@gmail.com>
2023-04-09 22:48:16 -0400

    Declare, initialize and interpolate variables

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string name = "Salim";
+            int age = 15;
+            char midInitial = 'N';
+            bool isHappy = true;
+            double doubleNum = 1.33d;
+            decimal decNum = 10.898m;
+
+            Console.WriteLine($"His name is {name} {midInitial} and he is {age} yaers old");
+            Console.WriteLine($"It's {isHappy} that the cat is happy");
+            Console.WriteLine($"Double value is {doubleNum} whereas decimal value is {decNum}");
         }
     }
 }
